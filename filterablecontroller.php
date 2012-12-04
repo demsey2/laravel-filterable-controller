899587e5918a9e0396ceb74927e625462e93e549
Kapil <kapv89@gmail.com>
2012-12-04 21:38:37 +0530

    added method parameters to filterable controller

--- filterablecontroller.php
+++ filterablecontroller.php
@@ -13,8 +13,8 @@ class FilterableController extends Controller
 			if( array_key_exists($this->method_filter_id($method), Filter::$filters) )
 				continue;
 
-			Filter::register($this->method_filter_id($method), function () use ($self, $method) {
-				return $self->$method();
+			Filter::register($this->method_filter_id($method), function ($params) use ($self, $method) {
+				return $self->$method($params);
 			});
 		}
 	}
@@ -30,24 +30,82 @@ class FilterableController extends Controller
 		return array_map(function ($m) use ($self) { return $self->method_filter_id($m); }, $methods);
 	}
 
-	public function method_filters($methods)
+	public function method_filter($event, $filters)
 	{
-		is_array($methods) or $methods = (array) $methods;
+		$methods = explode('|', $filters);
 		$this->make_method_filters($methods);
-
-		return implode('|', $this->method_filter_ids($methods));
+		
+		return $this->filter($event, implode('|', $this->method_filter_ids($methods)));
 	}
 
-	public function before_method_filter($filters)
+	/****Get method params for filters as assoc array**/
+	protected function method_params_assoc($method, $parameters)
 	{
-		return $this->filter('before', $this->method_filters($filters));
+		if ($this->restful)
+			$action = strtolower(Request::method()).'_'.$method;
+		else
+			$action = "action_{$method}";
+
+		$r = new ReflectionMethod(get_called_class(), $action);
+		
+		if(! $params = $r->getParameters() )
+			return array();
+
+		$data = array();
+
+		foreach($params as $i => $p) {
+			$key = $p->getName();
+			$val = isset($parameters[$i+1]) ?
+					$parameters[$i+1]
+				 : ($p->isDefaultValueAvailable() ? $p->getDefaultValue() : null);
+
+			$data[$key] = $val;
+		}
+
+		return $data;
 	}
 
-	public function after_method_filter($filters)
+	/**
+	 * Execute a controller method with the given parameters.
+	 * Method filters get the arguments of the method they are filtering
+	 * passed to them in the same order.
+	 * While normal filters don't get anything passed to them
+	 *
+	 * @param  string    $method
+	 * @param  array     $parameters
+	 * @return Response
+	 */
+	public function execute($method, $parameters = array())
 	{
-		return $this->filter('after', $this->method_filters($filters));
+		$filters = $this->filters('before', $method);
+
+		// Again, as was the case with route closures, if the controller "before"
+		// filters return a response, it will be considered the response to the
+		// request and the controller method will not be used.
+		// The parameters of the controller action will get passed to the filter
+		// in the form of an assoc array.
+
+		$params = $this->method_params_assoc($method, $parameters);
+		$params['action'] = $method;
+
+		$response = Filter::run($filters, array($params), true);
+
+		if (is_null($response))
+		{
+			$this->before();
+
+			$response = $this->response($method, $parameters);
+		}
+
+		$response = Response::prepare($response);
+
+		// The "after" function on the controller is simply a convenient hook
+		// so the developer can work on the response before it's returned to
+		// the browser. This is useful for templating, etc.
+		$this->after($response);
+
+		Filter::run($this->filters('after', $method), array($response, $params));
+
+		return $response;
 	}
-
-	/**********/
-
 }
---
+++ readme.md
@@ -0,0 +1,82 @@
+FilterableController allows controllers to register their own methods as filters.
+
+Credit for the idea goes to machuga. The implementation is my own.
+
+This helps with specially with ACL as a controller specific filters can exist in that 
+specific controller only and will be registered only when that controller is used.
+
+You need to keep the methods public (thats a limitation of php 5.3) and you can easily use
+$this in the methods.
+
+When using filterable controller, the before filters get passed the arguments of the current action and
+the current action as an assoc array. The same assoc array then is passed to the after filter as the 
+second argument (after response).
+
+For example, when a filter is applied to the action:
+	
+	public function get_show($section, $page = 'index')
+	{
+		...
+	}
+
+The following assoc array gets passed as argument to filters:
+	
+	array(
+		'section' => $section,
+		'page' => $page,
+		'action' => 'show'
+	)
+
+Full Usage:
+
+	//routes.php
+	Route::controller('items');
+
+	//controllers/items.php
+	class Items_Controller extends Controller
+	{
+		public $restful = true;
+
+		public function __construct()
+		{
+			$this->filter('before', 'auth');
+			$this->method_filter('before', 'user_active_check|role_check');
+			$this->method_filter('before', 'add_creator_to_input')->only(array('create', 'update'));
+			$this->method_filer('before', 'user_can_delete_item')->only('destroy');
+		}
+
+		public function user_active_check()
+		{
+			if( Auth::user()->is_active )
+				return Response::error(404);
+		}
+
+		public function role_check()
+		{
+			if(! Auth::user()->has_role('items-manager') )
+				return Response::error(404);
+		}
+
+		public function can_delete_item($params)
+		{
+			if(! Auth::user()->can_delete_item($params['id']) )
+				return Response::error(404);
+		}
+
+		public function log_activity($response)
+		{
+			ItemsManagerLogEntry::create(array(
+				'user_id' => Auth::user()->id, 'uri' => URI::current(), 'method' => Request::method()
+			));
+		}
+
+		public function add_creator_to_input()
+		{
+			Input::merge(array('creator_id' => Auth::user()->id));
+		}
+
+		/**actions**/
+		* Imagine you have your standard RESTful actions over here
+		* get_index(), get_show($id), get_new(), post_create(), get_edit($id), put_update($id), delete_destroy($id)
+		*/
+	}
